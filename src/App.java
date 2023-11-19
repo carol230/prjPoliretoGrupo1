faa63d264b6e16d439855599b3d53d3dc5569b9b
carol230 <caro.ayala2605@gmail.com>
2023-11-19 12:52:42 -0500

    C2 Base del proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //scanner funcion 
+        SerieSignosAlternos oSerieSignosAlternos = new SerieSignosAlternos();
+
+        System.out.println("Grupo: Uno");
+        System.out.println("Integrantes: Ivonne Ayala, ");
+
+    
     }
 }
---
+++ src/SerieSignosAlternos.java
@@ -0,0 +1,8 @@
+/*Copyright (C) 2K23, grupo1
+ * (grupo1Nombre), caro.ayala2605@gmail.com
+ * Version 1.0
+ */
+
+public class SerieSignosAlternos {
+    
+}
