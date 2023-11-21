e2cfaf75a4dc48dcd479720988f400ec92d7177c
Leandro Alava <kleyton.alava@epn.edu.ec>
2023-11-21 10:39:38 -0500

    S1-S2

--- src/App.java
+++ src/App.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        //scanner funcion 
-        SerieSignosAlternos oSerieSignosAlternos = new SerieSignosAlternos();
+        Scanner oSerieSignosAlternos = new Scanner(System.in);
 
         System.out.println("Grupo: Uno");
-        System.out.println("Integrantes: Ivonne Ayala, Leandor Alava, Lenin Amagandi, Alexis Bautista, Sebastian Bravo ");
+        System.out.println("Integrantes: Ivonne Ayala, Leandro Alava, Lenin Amagandi, Alexis Bautista, Sebastian Bravo ");
+        System.out.println("Ingrese el número de términos a generar ");
+        int n = oSerieSignosAlternos.nextInt();
 
-    
+        // Llamar a los métodos de la clase SerieSignosAlternos
+        SerieSignosAlternos.fibonacciConFor(n);
+        System.out.println("");
+        SerieSignosAlternos.fibonacciConDoWhile(n);
+
+        oSerieSignosAlternos.close();
+        
     }
+
 }
--- src/SerieSignosAlternos.java
+++ src/SerieSignosAlternos.java
@@ -1,8 +1,52 @@
 /*Copyright (C) 2K23, grupo1
- * (grupo1Nombre), caro.ayala2605@gmail.com
+ * (grupo1Nombre), kleyton.alava@epn.edu.ec
  * Version 1.0
  */
 
+import java.util.Scanner;        // "Scanner" se utilizará para leer la entrada del usuario.
+
 public class SerieSignosAlternos {
-    
+    public static void main(String[] args) {        //Metodo con el cual va ha iniciar el programa.
+        Scanner oSerieSignosAlternos = new Scanner(System.in);   // se crea un objeto llamado "scanner" para leer la entrada del usuario
+
+        // Pedirle al usuario que ingrese por el terminal el numero "n" de elementos que se van a presentar en la susecion
+        System.out.print("Ingrese el número de términos a generar: ");
+        int n = oSerieSignosAlternos.nextInt();
+
+        // Generar la serie Fibonacci usando un bucle for
+        System.out.println("Serie Fibonacci usando bucle for:");
+        fibonacciConFor(n);
+
+        // Generar la serie Fibonacci usando un bucle do-while
+        System.out.println("\nSerie Fibonacci usando bucle do-while:");
+        fibonacciConDoWhile(n);
+
+        oSerieSignosAlternos.close();        //Damos por cerrado el objeto
+    }
+
+    // Método para generar la serie Fibonacci usando un bucle for
+    public static void fibonacciConFor(int n) {
+        int a = 0, b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+
+    // Método para generar la serie Fibonacci usando un bucle do-while
+    public static void fibonacciConDoWhile(int n) {
+        int a = 0, b = 1;
+        int count = 0;
+
+        do {
+            System.out.print(a + " ");
+            int temp = a;
+            a = b;
+            b = temp + b;
+            count++;
+        } while (count < n);
+    }
 }
