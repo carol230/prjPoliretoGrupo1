b2a04446434c2979731389a8b1a200b8b03a901b
carol230 <caro.ayala2605@gmail.com>
2023-11-21 20:33:32 -0500

    Base codigo serie 5 y 6

--- src/App.java
+++ src/App.java
@@ -7,6 +7,11 @@ public class App {
         System.out.println("Grupo: Uno");
         System.out.println("Integrantes: Ivonne Ayala, Leandor Alava, Lenin Amagandi, Alexis Bautista, Sebastian Bravo ");
 
+        System.out.print("Serie 5: ");
+        oSerieSignosAlternos.showSerie5(9);
+        System.out.println();
+        System.out.print("Serie 6: ");
+        oSerieSignosAlternos.showSerie6(9);
     
     }
 }
--- src/SerieSignosAlternos.java
+++ src/SerieSignosAlternos.java
@@ -4,5 +4,63 @@
  */
 
 public class SerieSignosAlternos {
-    
+    /*
+     * showSerie muestra la serie
+     * @param NroTerminos : numero de terminos para la serie
+     */
+    public void showSerie5(int NroTerminos){
+        System.out.println();
+        System.out.print("Ciclo for: ");
+        for (int i=2;i<NroTerminos;++i){
+            if(i==2){
+                System.out.print(i+" ");
+            }else if(i%2!=0){
+                System.out.print(i+" ");
+                i++;
+            }
+        }
+        System.out.println();
+        System.out.print("Ciclo doWhile: ");
+        int i=2;
+        do {
+            if(i==2){
+                System.out.print(i+" ");
+            }else if(i%2!=0){
+                System.out.print(i+" ");
+            }
+            i++;
+        } while(i<NroTerminos);
+        System.out.println();
+        System.out.print("Ciclo While: ");
+        int j=2;
+        while(j<NroTerminos){
+            if(j==2){
+                System.out.print(j+" ");
+            }else if(j%2!=0){
+                System.out.print(j+" ");
+            }
+            j++;
+        }
+    }
+    public void showSerie6(int NroTerminos){
+        System.out.println();
+        System.out.print("Ciclo for: ");
+        for (int i=1;i<NroTerminos;++i){
+            System.out.print(i*i+" ");
+        }
+        System.out.println();
+        System.out.print("Ciclo doWhile: ");
+        int i=1;
+        do {
+            System.out.print(i*i+" ");
+            i++;
+        } while (i<NroTerminos);
+        System.out.println();
+        System.out.print("Ciclo While: ");
+        int j=1;
+        while(j<NroTerminos){
+            System.out.print(j*j+" ");
+            j++;
+        }
+    }
 }
